de3224f0e05c55c175b4774fdff164670ff98b23
brady <brady.wang@qq.com>
2021-06-15 20:50:18 +0800

    d

--- test.php
+++ test.php
@@ -2,6 +2,18 @@
 
 require_once "./vendor/autoload.php";
 
-use Brady\Test\Redis;
+use Brady\Token\JWTV1 as JWT;
 
-Redis::hello();
+
+
+//自己使用测试begin
+$payload_test = array('user_name'=>'brady');;
+$token_test = Jwt::getToken($payload_test);
+echo "<pre>";
+echo $token_test;
+
+//对token进行验证签名
+$getPayload_test = Jwt::verifyToken($token_test);
+echo "<br><br>";
+var_dump($getPayload_test);
+echo "<br><br>";
